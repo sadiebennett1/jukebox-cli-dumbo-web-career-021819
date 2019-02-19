fbc3e5b6d893616e9fba1ff997b88db04f883ade
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-19 02:31:51 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -29,7 +29,7 @@ def play(songs)
   puts "Please enter a song name or number:"
   song = gets.chomp
   nums = "1234567890"
-  if nums.include?(song)
+  if song.numeric?
     song_idx = song - 1
     title = songs[song_idx]
   else
